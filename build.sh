e9d383d4af861ca0f3e11c74d86b29fef6e42c28
Mohammad Rahhal <m.r992@hotmail.com>
2016-06-16 23:21:47 +0300

    Let's try this

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 dotnet --info
 dotnet restore
 dotnet build test/MR.AspNetCore.Jobs.SqlServer.Tests
-dotnet test test/MR.AspNetCore.Jobs.SqlServer.Tests
+dotnet test test/MR.AspNetCore.Jobs.SqlServer.Tests -f netcoreapp1.0
