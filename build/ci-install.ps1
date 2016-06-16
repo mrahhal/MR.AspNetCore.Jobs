2feff702fe2a9de338ee8b250f02bbfb223bb10a
Mohammad Rahhal <m.r992@hotmail.com>
2016-06-16 17:53:58 +0300

    Last push

--- build/ci-install.ps1
+++ build/ci-install.ps1
@@ -1,8 +1,10 @@
+cd build
+
 $dotnetVersion = "1.0.0-preview1-002702"
 $dotnetChannel = "beta"
 $dotnetLocalInstallFolder = "$env:LOCALAPPDATA\Microsoft\dotnet\"
 $newPath = "$dotnetLocalInstallFolder;$env:PATH"
-echo %cd%
+
 & "./dotnet-install.ps1" -Channel $dotnetChannel -Version $dotnetVersion -Architecture x64
 if (!($env:Path.Split(';') -icontains $dotnetLocalInstallFolder))
 {
--- build/ci-install.sh
+++ build/ci-install.sh
@@ -1,6 +1,8 @@
+cd build
+
 KOREBUILD_DOTNET_VERSION=1.0.0-preview1-002702
 KOREBUILD_DOTNET_CHANNEL=beta
-pwd
+
 chmod +x ./dotnet-install.sh
 
 ./dotnet-install.sh --channel $KOREBUILD_DOTNET_CHANNEL --version $KOREBUILD_DOTNET_VERSION
