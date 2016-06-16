89af8e6ea7e6567080bdac0e739c63c3175b8a8e
Mohammad Rahhal <m.r992@hotmail.com>
2016-06-16 15:22:56 +0300

    Abstract bootstrapping cron jobs to BootstrapperBase

--- src/MR.AspNetCore.Jobs.SqlServer/SqlServerBootstrapper.cs
+++ src/MR.AspNetCore.Jobs.SqlServer/SqlServerBootstrapper.cs
@@ -1,7 +1,4 @@
-using System;
-using System.Linq;
 using Microsoft.AspNetCore.Hosting;
-using MR.AspNetCore.Jobs.Models;
 using MR.AspNetCore.Jobs.Server;
 
 namespace MR.AspNetCore.Jobs
@@ -22,46 +19,6 @@ namespace MR.AspNetCore.Jobs
 
 		public override void BootstrapCore()
 		{
-			using (var connection = Storage.GetConnection())
-			{
-				var registry = Options.CronJobRegistry;
-				var entries = default(CronJobRegistry.Entry[]);
-				var existingJobs = connection.GetCronJobs();
-				if (registry != null &&
-					(entries = registry.Build()).Length != 0)
-				{
-					// Add or update jobs
-					foreach (var entry in entries)
-					{
-						var cronJob = existingJobs.FirstOrDefault(j => j.Name == entry.Name);
-						var updating = cronJob != null;
-						if (!updating)
-						{
-							cronJob = new CronJob
-							{
-								Name = entry.Name,
-								TypeName = entry.JobType.AssemblyQualifiedName,
-								Cron = entry.Cron,
-								LastRun = DateTime.MinValue
-							};
-							connection.StoreJob(cronJob);
-						}
-						else
-						{
-							cronJob.TypeName = entry.JobType.AssemblyQualifiedName;
-							cronJob.Cron = entry.Cron;
-							connection.UpdateCronJob(cronJob);
-						}
-					}
-				}
-
-				// Delete old jobs
-				foreach (var oldJob in existingJobs.Where(j => !entries.Any(e => e.Name == j.Name)))
-				{
-					connection.RemoveCronJob(oldJob.Name);
-				}
-			}
-
 			_appLifetime.ApplicationStopping.Register(() => Server.Dispose());
 		}
 	}
--- src/MR.AspNetCore.Jobs/BootstrapperBase.cs
+++ src/MR.AspNetCore.Jobs/BootstrapperBase.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Linq;
+using MR.AspNetCore.Jobs.Models;
 using MR.AspNetCore.Jobs.Server;
 
 namespace MR.AspNetCore.Jobs
@@ -23,10 +26,55 @@ namespace MR.AspNetCore.Jobs
 		public void Bootstrap()
 		{
 			Storage.Initialize();
-
+			WorkOutCronJobs();
 			BootstrapCore();
-
 			Server.Start();
+		}
+
+		public void WorkOutCronJobs()
+		{
+			var entries = Options.CronJobRegistry?.Build() ?? Enumerable.Empty<CronJobRegistry.Entry>().ToArray();
+			using (var connection = Storage.GetConnection())
+			{
+				var currentJobs = connection.GetCronJobs();
+				WorkOutCronJobsCore(connection, entries, currentJobs);
+			}
+		}
+
+		public virtual void WorkOutCronJobsCore(IStorageConnection connection, CronJobRegistry.Entry[] entries, CronJob[] currentJobs)
+		{
+			if (entries.Length != 0)
+			{
+				// Add or update jobs
+				foreach (var entry in entries)
+				{
+					var cronJob = currentJobs.FirstOrDefault(j => j.Name == entry.Name);
+					var updating = cronJob != null;
+					if (!updating)
+					{
+						cronJob = new CronJob
+						{
+							Name = entry.Name,
+							TypeName = entry.JobType.AssemblyQualifiedName,
+							Cron = entry.Cron,
+							LastRun = DateTime.MinValue
+						};
+						connection.StoreJob(cronJob);
+					}
+					else
+					{
+						cronJob.TypeName = entry.JobType.AssemblyQualifiedName;
+						cronJob.Cron = entry.Cron;
+						connection.UpdateCronJob(cronJob);
+					}
+				}
+			}
+
+			// Delete old jobs
+			foreach (var oldJob in currentJobs.Where(j => !entries.Any(e => e.Name == j.Name)))
+			{
+				connection.RemoveCronJob(oldJob.Name);
+			}
 		}
 
 		public virtual void BootstrapCore()
