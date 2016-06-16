25934d57a68aa569572cad65a1032a55791d4501
Mohammad Rahhal <m.r992@hotmail.com>
2016-06-16 19:07:51 +0300

    No need for dotnet install scripts

--- build/ci-install.sh
+++
@@ -1,6 +0,0 @@
-KOREBUILD_DOTNET_VERSION=1.0.0-preview1-002702
-KOREBUILD_DOTNET_CHANNEL=beta
-
-chmod +x ./build/dotnet-install.sh
-
-./build/dotnet-install.sh --channel $KOREBUILD_DOTNET_CHANNEL --version $KOREBUILD_DOTNET_VERSION
