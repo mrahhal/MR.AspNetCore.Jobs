77e7d97ffc328d3895196fca9506392c165ba8ed
Mohammad Rahhal <m.r992@hotmail.com>
2016-06-16 22:50:44 +0300

    Let's try this

--- build.sh
+++ build.sh
@@ -1,1 +1,7 @@
-xbuild build/Build.proj /p:BuildType=Release
+#xbuild build/Build.proj /p:BuildType=Release
+
+dotnet --info
+dotnet build src/MR.AspNetCore.Jobs
+dotnet build src/MR.AspNetCore.Jobs.SqlServer
+dotnet build test/MR.AspNetCore.Jobs.SqlServer.Tests
+dotnet test test/MR.AspNetCore.Jobs.SqlServer.Tests
