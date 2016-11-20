1edc4996fd66230c8cef7487f74ce912e66198fd
Mohammad Rahhal <m.r992@hotmail.com>
2016-11-20 08:03:29 +0200

    + more tests

--- test/MR.AspNetCore.Jobs.SqlServer.Tests/SqlServerStorageConnectionTest.cs
+++ test/MR.AspNetCore.Jobs.SqlServer.Tests/SqlServerStorageConnectionTest.cs
@@ -5,21 +5,35 @@ using Dapper;
 using FluentAssertions;
 using Microsoft.Extensions.DependencyInjection;
 using MR.AspNetCore.Jobs.Models;
+using MR.AspNetCore.Jobs.Server.States;
 using Xunit;
 
 namespace MR.AspNetCore.Jobs
 {
 	public class SqlServerStorageConnectionTest : DatabaseTestHost
 	{
+		private IServiceProvider _provider;
+
+		public SqlServerStorageConnectionTest()
+		{
+			var services = new ServiceCollection();
+			services.AddOptions();
+			services.AddLogging();
+			services.AddTransient<SqlServerStorage>();
+			services.Configure<SqlServerOptions>(
+				options => options.ConnectionString = ConnectionUtil.GetConnectionString());
+			_provider = services.BuildServiceProvider();
+		}
+
 		[Fact]
-		public async Task StoreJob_DelayedJob()
+		public async Task StoreJobAsync()
 		{
 			// Arrange
 			var fixture = Create();
-			var model = new Job("data", new DateTime(2000, 1, 1));
+			var job = new Job("data", new DateTime(2000, 1, 1));
 
 			// Act
-			await fixture.StoreJobAsync(model);
+			await fixture.StoreJobAsync(job);
 
 			// Assert
 			fixture._storage.UseConnection(connection =>
@@ -29,58 +43,36 @@ namespace MR.AspNetCore.Jobs
 		}
 
 		[Fact]
-		public async Task StoreJob_CronJob()
+		public async Task FetchNextJobAsync_ReadsPast()
 		{
 			// Arrange
 			var fixture = Create();
-			var model = new CronJob
-			{
-				Cron = Cron.Daily(),
-				Name = "1",
-				TypeName = "foo",
-				LastRun = new DateTime(2000, 1, 1)
-			};
+			var job1 = new Job("data");
+			var job2 = new Job("data");
+			await fixture.StoreJobAsync(job1);
+			await fixture.StoreJobAsync(job2);
 
-			// Act
-			await fixture.StoreCronJobAsync(model);
-
-			// Assert
 			fixture._storage.UseConnection(connection =>
 			{
-				connection.Query<CronJob>("SELECT * FROM [Jobs].CronJobs").Count().Should().NotBe(0);
+				connection.Execute("INSERT INTO [Jobs].JobQueue (JobId) VALUES (@Id)", new[] { job1, job2 });
+			});
+
+			// Act
+			var fJob1 = await fixture.FetchNextJobAsync();
+			var fJob2 = await fixture.FetchNextJobAsync();
+			fJob1.RemoveFromQueue();
+			fJob2.RemoveFromQueue();
+
+			// Assert
+			fJob1.JobId.Should().NotBe(fJob2.JobId);
+			fixture._storage.UseConnection(connection =>
+			{
+				connection.Query<Job>("SELECT * FROM [Jobs].JobQueue").Count().Should().Be(0);
 			});
 		}
 
 		[Fact]
-		public async Task UpdateCronJob()
-		{
-			// Arrange
-			var fixture = Create();
-			var model = new CronJob
-			{
-				Cron = Cron.Daily(),
-				Name = "1",
-				TypeName = "foo",
-				LastRun = new DateTime(2000, 1, 1)
-			};
-			await fixture.StoreCronJobAsync(model);
-			model.LastRun = new DateTime(2001, 1, 1);
-			model.Cron = Cron.Minutely();
-
-			// Act
-			await fixture.UpdateCronJobAsync(model);
-
-			// Assert
-			fixture._storage.UseConnection(connection =>
-			{
-				var job = connection.Query<CronJob>("SELECT * FROM [Jobs].CronJobs").First();
-				job.LastRun.Should().Be(model.LastRun);
-				job.Cron.Should().Be(model.Cron);
-			});
-		}
-
-		[Fact]
-		public async Task FetchNextDelayedJobAsync_NoJobs_ReturnsNull()
+		public async Task FetchNextJobAsync_NoJobs_ReturnsNull()
 		{
 			// Arrange
 			var fixture = Create();
@@ -93,15 +85,15 @@ namespace MR.AspNetCore.Jobs
 		}
 
 		[Fact]
-		public async Task FetchNextDelayedJobAsync_Commit()
+		public async Task FetchNextJobAsync_DueMinValue_Commit()
 		{
 			// Arrange
 			var fixture = Create();
-			var model = new Job("data");
-			await fixture.StoreJobAsync(model);
+			var job = new Job("data", DateTime.MinValue);
+			await fixture.StoreJobAsync(job);
 			using (var t = fixture.CreateTransaction())
 			{
-				t.EnqueueJob(model.Id);
+				t.EnqueueJob(job.Id);
 				await t.CommitAsync();
 			}
 
@@ -114,39 +106,16 @@ namespace MR.AspNetCore.Jobs
 			(await fixture.FetchNextJobAsync()).Should().BeNull();
 		}
 
-		// Requeuing won't work directly here because we're using a transaction scope
-		// to clean the database.
-		//[Fact]
-		//public void FetchNextJob_Requeue()
-		//{
-		//	// Arrange
-		//	var fixture = Create();
-		//	var model = new DelayedJob
-		//	{
-		//		Data = "data",
-		//		Due = null
-		//	};
-		//	fixture.StoreJob(model);
-
-		//	// Act
-		//	var result = fixture.FetchNextJob();
-		//	result.Requeue();
-		//	result.Dispose();
-
-		//	// Assert
-		//	fixture.FetchNextJob().Should().NotBeNull();
-		//}
-
 		[Fact]
-		public async Task FetchNextDelayedJobAsync_DueMinValue_Commit()
+		public async Task FetchNextJobAsync_Commit()
 		{
 			// Arrange
 			var fixture = Create();
-			var model = new Job("data", DateTime.MinValue);
-			await fixture.StoreJobAsync(model);
+			var job = new Job("data");
+			await fixture.StoreJobAsync(job);
 			using (var t = fixture.CreateTransaction())
 			{
-				t.EnqueueJob(model.Id);
+				t.EnqueueJob(job.Id);
 				await t.CommitAsync();
 			}
 
@@ -159,48 +128,148 @@ namespace MR.AspNetCore.Jobs
 			(await fixture.FetchNextJobAsync()).Should().BeNull();
 		}
 
-		//[Fact]
-		//public void FetchNextDelayedJob_Requeue()
-		//{
-		//	// Arrange
-		//	var fixture = Create();
-		//	var model = new DelayedJob
-		//	{
-		//		Data = "data",
-		//		Due = DateTime.MinValue
-		//	};
-		//	fixture.StoreJob(model);
-
-		//	// Act
-		//	var result = fixture.FetchNextDelayedJob();
-		//	result.Requeue();
-		//	result.Dispose();
-
-		//	// Assert
-		//	fixture.FetchNextDelayedJob().Should().NotBeNull();
-		//}
-
 		[Fact]
-		public async Task GetCronJobs()
+		public async Task GetNextJobToBeEnqueuedAsync()
 		{
 			// Arrange
 			var fixture = Create();
-			var model1 = new CronJob
+			var job = new Job("data", DateTime.UtcNow.Subtract(TimeSpan.FromSeconds(10)));
+			await fixture.StoreJobAsync(job);
+
+			// Act
+			var jobToBeEnqueued = await fixture.GetNextJobToBeEnqueuedAsync();
+
+			// Assert
+			jobToBeEnqueued.Id.Should().Be(job.Id);
+		}
+
+		[Fact]
+		public async Task GetNextJobToBeEnqueuedAsync_NotDue()
+		{
+			// Arrange
+			var fixture = Create();
+			var job = new Job("data", DateTime.UtcNow.Add(TimeSpan.FromDays(1)));
+			await fixture.StoreJobAsync(job);
+
+			// Act
+			var jobToBeEnqueued = await fixture.GetNextJobToBeEnqueuedAsync();
+
+			// Assert
+			jobToBeEnqueued.Should().BeNull();
+		}
+
+		[Fact]
+		public async Task GetNextJobToBeEnqueuedAsync_InAStateOtherThanScheduled()
+		{
+			// Arrange
+			var fixture = Create();
+			var job = new Job("data", DateTime.UtcNow.Subtract(TimeSpan.FromSeconds(10)));
+			job.StateName = EnqueuedState.StateName;
+			await fixture.StoreJobAsync(job);
+
+			// Act
+			var jobToBeEnqueued = await fixture.GetNextJobToBeEnqueuedAsync();
+
+			// Assert
+			jobToBeEnqueued.Should().BeNull();
+		}
+
+		[Fact]
+		public async Task StoreCronJobAsync()
+		{
+			// Arrange
+			var fixture = Create();
+			var cronJob = new CronJob
 			{
 				Cron = Cron.Daily(),
 				Name = "1",
 				TypeName = "foo",
 				LastRun = new DateTime(2000, 1, 1)
 			};
-			var model2 = new CronJob
+
+			// Act
+			await fixture.StoreCronJobAsync(cronJob);
+
+			// Assert
+			fixture._storage.UseConnection(connection =>
+			{
+				connection.Query<CronJob>("SELECT * FROM [Jobs].CronJobs").Count().Should().NotBe(0);
+			});
+		}
+
+		[Fact]
+		public async Task UpdateCronJobAsync()
+		{
+			// Arrange
+			var fixture = Create();
+			var cronJob = new CronJob
+			{
+				Cron = Cron.Daily(),
+				Name = "1",
+				TypeName = "foo",
+				LastRun = new DateTime(2000, 1, 1)
+			};
+			await fixture.StoreCronJobAsync(cronJob);
+			cronJob.LastRun = new DateTime(2001, 1, 1);
+			cronJob.Cron = Cron.Minutely();
+
+			// Act
+			await fixture.UpdateCronJobAsync(cronJob);
+
+			// Assert
+			fixture._storage.UseConnection(connection =>
+			{
+				var job = connection.Query<CronJob>("SELECT * FROM [Jobs].CronJobs").First();
+				job.LastRun.Should().Be(cronJob.LastRun);
+				job.Cron.Should().Be(cronJob.Cron);
+			});
+		}
+
+		// Requeuing won't work directly here because we're using a transaction scope
+		// to clean the database.
+		/*[Fact]
+		public async Task FetchNextJob_Requeue()
+		{
+			// Arrange
+			var fixture = Create();
+			var job = new Job("data", DateTime.UtcNow.Subtract(TimeSpan.FromSeconds(10)));
+			await fixture.StoreJobAsync(job);
+
+			fixture._storage.UseConnection(connection =>
+			{
+				connection.Execute("INSERT INTO [Jobs].JobQueue (JobId) VALUES (@Id)", new[] { job });
+			});
+
+			// Act
+			var result = await fixture.FetchNextJobAsync();
+			result.Requeue();
+			result.Dispose();
+
+			// Assert
+			(await fixture.FetchNextJobAsync()).Should().NotBeNull();
+		}*/
+
+		[Fact]
+		public async Task GetCronJobsAsync()
+		{
+			// Arrange
+			var fixture = Create();
+			var cronJob1 = new CronJob
+			{
+				Cron = Cron.Daily(),
+				Name = "1",
+				TypeName = "foo",
+				LastRun = new DateTime(2000, 1, 1)
+			};
+			var cronJob2 = new CronJob
 			{
 				Cron = Cron.Minutely(),
 				Name = "2",
 				TypeName = "bar",
 				LastRun = new DateTime(2000, 1, 2)
 			};
-			await fixture.StoreCronJobAsync(model1);
-			await fixture.StoreCronJobAsync(model2);
+			await fixture.StoreCronJobAsync(cronJob1);
+			await fixture.StoreCronJobAsync(cronJob2);
 
 			// Act
 			var result = await fixture.GetCronJobsAsync();
@@ -211,13 +280,7 @@ namespace MR.AspNetCore.Jobs
 
 		private SqlServerStorageConnection Create()
 		{
-			var services = new ServiceCollection();
-			services.AddOptions();
-			services.AddLogging();
-			services.AddTransient<SqlServerStorage>();
-			services.Configure<SqlServerOptions>(
-				options => options.ConnectionString = ConnectionUtil.GetConnectionString());
-			return services.BuildServiceProvider()
+			return _provider
 				.GetService<SqlServerStorage>()
 				.GetConnection() as SqlServerStorageConnection;
 		}
---
+++ test/MR.AspNetCore.Jobs.Tests/Server/ProcessingServerTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Xunit;
+
+namespace MR.AspNetCore.Jobs.Server
+{
+	public class ProcessingServerTest
+	{
+		private Mock<IStorage> _mockStorage;
+		private JobsOptions _options;
+		private IServiceProvider _provider;
+
+		public ProcessingServerTest()
+		{
+			_options = new JobsOptions()
+			{
+				PollingDelay = 0
+			};
+			_mockStorage = new Mock<IStorage>();
+
+			var services = new ServiceCollection();
+			services.AddTransient<ProcessingServer>();
+			services.AddLogging();
+			services.AddSingleton(_options);
+			services.AddSingleton(_mockStorage.Object);
+			_provider = services.BuildServiceProvider();
+		}
+
+		public ProcessingServer Create() => _provider.GetService<ProcessingServer>();
+	}
+}
