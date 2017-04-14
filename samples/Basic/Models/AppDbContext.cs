e6d9c83647e22f79f893e724037465b694a52261
Mohammad Rahhal <m.r992@hotmail.com>
2017-04-14 16:57:39 +0300

    samples: minor *

--- samples/Basic/Models/AppDbContext.cs
+++ samples/Basic/Models/AppDbContext.cs
@@ -4,10 +4,6 @@ namespace Basic.Models
 {
 	public class AppDbContext : DbContext
 	{
-		public AppDbContext()
-		{
-		}
-
 		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
 		{
 		}
