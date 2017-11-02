656a3272d68585f6f9b88121bd378d42b38e05c1
Mohammad Rahhal <m.r992@hotmail.com>
2017-11-02 09:03:24 +0200

    chore: fix build

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 dotnet --info
 dotnet restore
-dotnet test test/MR.AspNetCore.Jobs.Tests/MR.AspNetCore.Jobs.Tests.csproj -f netcoreapp1.1
+dotnet test test/MR.AspNetCore.Jobs.Tests/MR.AspNetCore.Jobs.Tests.csproj -f netcoreapp2.0
