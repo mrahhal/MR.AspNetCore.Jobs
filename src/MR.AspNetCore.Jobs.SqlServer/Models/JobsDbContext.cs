375c50cf170bdcb0ed55a5c9a97b6befec3be40e
Mohammad Rahhal <m.r992@hotmail.com>
2017-12-09 09:40:32 +0200

    Move schema config to concrete contexts

--- src/MR.AspNetCore.Jobs.SqlServer/Models/JobsDbContext.cs
+++ src/MR.AspNetCore.Jobs.SqlServer/Models/JobsDbContext.cs
@@ -17,5 +17,25 @@ namespace MR.AspNetCore.Jobs.Models
 		{
 			_sqlServerOptions = sqlServerOptions;
 		}
+
+		protected override void OnModelCreating(ModelBuilder builder)
+		{
+			builder.HasDefaultSchema(RelationalOptions.Schema);
+
+			builder.Entity<Job>(b =>
+			{
+				b.Property(x => x.StateName).IsRequired();
+
+				b.HasIndex(x => new { x.Due, x.StateName });
+				b.HasIndex(x => x.StateName);
+			});
+
+			builder.Entity<CronJob>(b =>
+			{
+				b.Property(x => x.Name).IsRequired();
+
+				b.HasIndex(x => x.Name).IsUnique();
+			});
+		}
 	}
 }
