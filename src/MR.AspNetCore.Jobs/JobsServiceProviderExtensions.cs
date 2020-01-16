3606a2c3120bf26148beb0ca46b728950056874a
Mohammad Rahhal <m.r992@hotmail.com>
2020-01-16 19:49:28 +0200

    Change StartJobsAsync to work on the generic host

--- src/MR.AspNetCore.Jobs/JobsServiceProviderExtensions.cs
+++ src/MR.AspNetCore.Jobs/JobsServiceProviderExtensions.cs
@@ -1,12 +1,12 @@
 ﻿using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace MR.AspNetCore.Jobs
 {
 	public static class JobsWebHostExtensions
 	{
-		public static Task StartJobsAsync(this IWebHost host)
+		public static Task StartJobsAsync(this IHost host)
 		{
 			var bootstrapper = host.Services.GetRequiredService<IBootstrapper>();
 			return bootstrapper.BootstrapAsync();
